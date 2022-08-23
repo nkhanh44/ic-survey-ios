fbdc6e7851343d403f81fdc1e4d69407839d8fd1
nkhanh44 <nkhanh4496@gmail.com>
2022-08-23 10:22:26 +0700

    [#26] Implement renew access token

--- Survey/Sources/Data/NetworkAPI/Interceptors/AuthenticationInterceptor.swift
+++ Survey/Sources/Data/NetworkAPI/Interceptors/AuthenticationInterceptor.swift
@@ -8,10 +8,15 @@
 
 import Alamofire
 import Foundation
+import Japx
+import SwiftUI
 
 final class AuthenticationInterceptor: RequestInterceptor {
 
+    @EnvironmentObject private var appRouter: AppRouter
+
     private var keychain: KeychainServiceProtocol
+    private var refreshRequest: AFRequest?
 
     init(keychain: KeychainServiceProtocol = KeychainService.shared) {
         self.keychain = keychain
@@ -28,6 +33,63 @@ final class AuthenticationInterceptor: RequestInterceptor {
             completion(.success(request))
         } else {
             completion(.failure(NetworkAPIError.generic))
+        }
+    }
+
+    func retry(
+        _ request: Alamofire.Request,
+        for session: Session,
+        dueTo error: Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        guard request.retryCount == 0 else { return completion(.doNotRetry) }
+
+        if refreshRequest != nil { return }
+
+        refreshRequest = getRefreshToken { [weak self] result in
+            switch result {
+            case let .success(authToken):
+                try? self?.keychain.set(newToken: KeychainToken(token: authToken), for: .token)
+                completion(.retry)
+            case let .failure(error):
+                completion(.doNotRetryWithError(error))
+            }
+
+            self?.refreshRequest = nil
+        }
+    }
+
+    private func getRefreshToken(completion: @escaping (Result<Token, Error>) -> Void) -> AFRequest? {
+        let path = "\(Constants.API.baseUrl)oauth/token"
+        guard let refreshToken = try? keychain.get(key: .token)?.refreshToken else {
+            completion(.failure(NetworkAPIError.generic))
+            return nil
+        }
+        return AF.request(
+            path,
+            method: .post,
+            parameters: [
+                "grant_type": "refresh_token",
+                "refresh_token": refreshToken,
+                "client_id": Constants.Keys.clientId,
+                "client_secret": Constants.Keys.clientSecret
+            ]
+        )
+        .responseData { data in
+            do {
+                switch data.result {
+                case let .success(data):
+                    let result = try JapxDecoder().decode(
+                        JapxResponse<APIToken>.self,
+                        from: data
+                    )
+                    completion(.success(result.data))
+                case .failure:
+                    self.appRouter.state = .login
+                }
+            } catch {
+                completion(.failure(error))
+            }
         }
     }
 }

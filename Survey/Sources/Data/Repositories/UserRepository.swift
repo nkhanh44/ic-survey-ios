f9cf9606fcbb52a89659e4abfa0356659f2b6152
nkhanh44 <nkhanh4496@gmail.com>
2022-09-11 19:31:38 +0700

    [#36] Implement signout service

---
+++ Survey/Sources/Data/Repositories/UserRepository.swift
@@ -0,0 +1,37 @@
+//
+//  UserRepository.swift
+//  Survey
+//
+//  Created by Khanh on 11/09/2022.
+//  Copyright © 2022 Nimble. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+protocol UserRepositoryProtocol: AnyObject {
+
+    func getUser() -> Observable<User>
+    func logout() -> Observable<Bool>
+}
+
+final class UserRepository: UserRepositoryProtocol {
+
+    private var networkAPI: NetworkAPIProtocol!
+
+    required init(api: NetworkAPIProtocol) {
+        networkAPI = api
+    }
+
+    func getUser() -> Observable<User> {
+        networkAPI.performRequest(UserRequestConfiguration.getUser())
+            .map { $0 as APIUser }
+            .eraseToAnyPublisher()
+    }
+
+    func logout() -> Observable<Bool> {
+        networkAPI.performRequest(UserRequestConfiguration.logout())
+            .map { $0 as Bool }
+            .eraseToAnyPublisher()
+    }
+}
